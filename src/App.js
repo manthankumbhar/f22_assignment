70cfd412d03269c772c9172e2598ddeb4ab26276
manthankumbhar <kum.manthan@gmail.com>
2022-07-21 10:26:21 +0530

    form add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import "./App.scss";
+import Form from "./components/Form/Form";
 import Landing from "./components/Landing/Landing";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Landing />} />
+        <Route path="/signup" element={<Form />} />
       </Routes>
     </div>
   );
